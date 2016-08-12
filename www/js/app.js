a9e3cbfa1cad4ed623766da643029ed6aed401c4
Matheus Pessoa <matheusmpessoa17@gmail.com>
2016-08-12 18:20:14 -0300

    ID ionic criada. Navegação artistas alterada. Imgs alteradas

--- www/js/app.js
+++ www/js/app.js
@@ -3,83 +3,124 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic'])
 
 .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
-
         }
         if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
     });
 })
 
+
 .config(function ($stateProvider, $urlRouterProvider) {
-
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
+        .state('tabs', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+        })
 
-    // setup an abstract state for the tabs directive
-        .state('tab', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-    })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-        url: '/dash',
+    .state('tabs.home', {
+        url: '/home',
         views: {
-            'tab-dash': {
-                templateUrl: 'templates/tab-dash.html',
-                controller: 'DashCtrl'
+            'home-tab': {
+                templateUrl: 'templates/home.html'
             }
         }
     })
 
-    .state('tab.artists', {
-            url: '/artists',
-            views: {
-                'tab-artists': {
-                    templateUrl: 'templates/tab-artists.html',
-                    controller: 'ArtistsCtrl'
-                }
-            }
-        })
-        .state('tab.artist-detail', {
-            url: '/artists/:artistId',
-            views: {
-                'tab-artists': {
-                    templateUrl: 'templates/artist-detail.html',
-                    controller: 'ArtistDetailCtrl'
-                }
-            }
-        })
-
-    .state('tab.account', {
-        url: '/account',
+    .state('tabs.list', {
+        url: '/list',
         views: {
-            'tab-account': {
-                templateUrl: 'templates/tab-account.html',
-                controller: 'AccountCtrl'
+            'list-tab': {
+                templateUrl: 'templates/list.html',
+                controller: 'ListController'
             }
         }
-    });
+    })
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/tab/dash');
+    .state('tabs.detail', {
+        url: '/list/:aId',
+        views: {
+            'list-tab': {
+                templateUrl: 'templates/detail.html',
+                controller: 'ListController'
+            }
+        }
+    })
 
-});
+    .state('tabs.calendar', {
+        url: '/calendar',
+        views: {
+            'calendar-tab': {
+                templateUrl: 'templates/calendar.html',
+                controller: 'CalendarController'
+            }
+        }
+    })
+
+
+    $urlRouterProvider.otherwise('/tab/home');
+})
+
+.controller('CalendarController', ['$scope', '$http', '$state',
+    function ($scope, $http, $state) {
+        $http.get('js/data.json').success(function (data) {
+            $scope.calendar = data.calendar;
+
+            $scope.onItemDelete = function (dayIndex, item) {
+                $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
+            }
+
+            $scope.doRefresh = function () {
+                $http.get('js/data.json').success(function (data) {
+                    $scope.calendar = data.calendar;
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+            }
+
+            $scope.toggleStar = function (item) {
+                item.star = !item.star;
+            }
+
+        });
+}])
+
+.controller('ListController', ['$scope', '$http', '$state',
+    function ($scope, $http, $state) {
+        $http.get('js/data.json').success(function (data) {
+            $scope.artists = data.artists;
+            $scope.whichartist = $state.params.aId;
+            $scope.data = {
+                showDelete: false,
+                showReorder: false
+            };
+
+            $scope.onItemDelete = function (item) {
+                $scope.artists.splice($scope.artists.indexOf(item), 1);
+            }
+
+            $scope.doRefresh = function () {
+                $http.get('js/data.json').success(function (data) {
+                    $scope.artists = data;
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+            }
+
+            $scope.toggleStar = function (item) {
+                item.star = !item.star;
+            }
+
+            $scope.moveItem = function (item, fromIndex, toIndex) {
+                $scope.artists.splice(fromIndex, 1);
+                $scope.artists.splice(toIndex, 0, item);
+            };
+        });
+}]);
